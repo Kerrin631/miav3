868297ccebbb0660a77bc16933f408f97437fdc2
Kerrin Arora <kerrin.arora@gmail.com>
2016-10-20 22:20:31 -0500

    added procedure types pages and created api calls to SSCMS

--- client/angular/controllers/procedureTypeController.js
+++ client/angular/controllers/procedureTypeController.js
@@ -4,11 +4,20 @@ myAppModule.controller('procedureTypeController', function ($scope, $state, $rou
 	// console.log($routeParams.procedureType)
 	$scope.procedureType = $routeParams.procedureType
 
-	// $scope.getHeader = function(){
-		// data = $scope.procedureType
 		procedureFactory.getHeader($scope.procedureType, function(data) {
 			$scope.headerImg = data;
 		})
-	// }
+
+	if ($routeParams.procedureType == 'bodyProcedures') {
+		$scope.type = 'BODY PROCEDURES'
+	} else if ($routeParams.procedureType == 'faceProcedures') {
+		$scope.type = 'FACIAL PROCEDURES'
+	} else if ($routeParams.procedureType == 'skinProcedures') {
+		$scope.type = 'SKIN PROCEDURES'
+	} else if ($routeParams.procedureType == 'breastsProcedures') {
+		$scope.type = 'BREAST PROCEDURES'
+	}
+
+
 
 })
